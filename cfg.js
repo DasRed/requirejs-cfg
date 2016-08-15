c9eb5784108403b58484ebb81fa2037329485be3
Marco Starker <marco.starker@posterxxl.com>
2016-08-15 10:51:44 +0200

    bugfix

--- cfg.js
+++ cfg.js
@@ -1,29 +1,6 @@
 'use strict';
 
 define(['config-loader'], function (ConfigLoader) {
-    // Polyfill Object.assign
-    if (typeof Object.assign != 'function') {
-        Object.assign = function (target) {
-            'use strict';
-            if (target == null) {
-                throw new TypeError('Cannot convert undefined or null to object');
-            }
-
-            target = Object(target);
-            for (var index = 1; index < arguments.length; index++) {
-                var source = arguments[index];
-                if (source != null) {
-                    for (var key in source) {
-                        if (Object.prototype.hasOwnProperty.call(source, key)) {
-                            target[key] = source[key];
-                        }
-                    }
-                }
-            }
-            return target;
-        };
-    }
-
     return {
         /**
          * @param {String} name
@@ -50,9 +27,6 @@ define(['config-loader'], function (ConfigLoader) {
             }
             catch (exception) {
                 console.error(exception.message);
-            }
-            finally {
-                loadedConfig = Object.assign({}, loadedConfig);
             }
 
             console.debug('Config loaded from #' + name);
